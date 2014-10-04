e3188ba7e2f7b9c7a8f0a92d042d9154e51e6eff
Alec <spirescualex@gmail.com>
2014-10-04 14:23:15 +0300

    a doua modificare
    Am corectat ceva. Am adaugat linii de cod.

--- joc/Plugin.java
+++ joc/Plugin.java
@@ -3,6 +3,11 @@ package joc;
 public class Plugin.java{
 
 	public static void main(String[] args){
-		System.out.prinltn("Salut, eu sunt un plugin. Voi fi barosan!");
+		System.out.println("Salut, eu sunt un plugin. Voi fi barosan!");
+		System.out.println("Beginning game");
+		for(int i=0; i<20; i++){
+			System.out.prinltn("*playing*");
+		}
+		System.out.println("END game")
 	}
 }
